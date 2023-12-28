5300539c7a97a3670751eaecdd9526e82e77fe97
Resul Can Aksu <resulcanaksu@gmx.de>
2023-12-28 22:30:06 +0100

    fin. normal, start elim. AND for cnf

--- project/sat_solver.pl
+++ project/sat_solver.pl
@@ -3,8 +3,77 @@
 :- load_test_files([]).
 
 %% normalise(+Formula, -NFormula).
-normalise(_Formula, _NFormula) :-
-    true.
+%% basecase
+
+normalise(lit(X), X) 
+    :-!.
+
+normalise(not(and(X,Y)), Norm)
+    :- !, normalise(or(not(X),not(Y)), Norm).
+
+normalise(not(or(X,Y)), Norm)
+    :- !, normalise(and(not(X),not(Y)), Norm).
+
+normalise(not(not(X)), NormalisedX) 
+    :- !, normalise(X, NormalisedX).    
+
+%% negation
+normalise(not(X), not(NX)) 
+    :- !, normalise(X,NX).
+
+%% conjunction
+normalise(and(X,Y), and(NX,NY)) 
+    :- !, normalise(X,NX), normalise(Y,NY).
+
+%% disjunction
+normalise(or(X, Y), or(NX, NY)) 
+    :- !, normalise(X, NX), normalise(Y, NY).
+
+%% implication
+normalise(implies(X, Y), or(NX, NY)) 
+    :- !, normalise(not(X), NX), normalise(Y, NY).
+
+%% equivalence
+normalise(equivalence(X, Y), and(NX, NY)) 
+    :- !, normalise(implies(X,Y), NX),
+       normalise(implies(Y,X), NY).
+
+%% min_one_pos
+normalise(min_one_pos(Vars), NormalisedForm)
+    :- list_to_disjunction(Vars,Disjunction),
+        normalise(Disjunction, NormalisedForm).
+
+%% exactly_one_pos
+normalise(exactly_one_pos(Literals), NormalisedForm) :-
+    findall(OrClause, pairwise_negation(Literals, OrClause), PairwiseNegations),
+    list_to_disjunction(Literals, AtLeastOne),
+    append(PairwiseNegations, [AtLeastOne], AllClauses),
+    list_to_conjunction(AllClauses, Conjunctionss),
+    normalise(Conjunctionss, NormalisedForm).
+
+%% Create pairwise negation clauses
+pairwise_negation(Literals, OrClause) :-
+    select(Lit1, Literals, Remaining),
+    member(Lit2, Remaining),
+    Lit1 @< Lit2,
+    OrClause = or(not(Lit1), not(Lit2)).
+
+% Helper
+negate(X, NX) 
+    :- !, normalise(not(lit(X)), NX).
+
+list_to_conjunction([X], X).
+list_to_conjunction([X|Xs], and(X, Conjunction)) 
+    :-list_to_conjunction(Xs, Conjunction).
+
+list_to_disjunction([X], X).
+list_to_disjunction([X|Xs], or(X, Disjunction)) 
+    :-list_to_disjunction(Xs, Disjunction).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% TODO: Eliminate ANDs
 
 %% to_cnf(+Formula, -CNF).
 to_cnf(_Formula, _CNF) :-
